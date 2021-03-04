7ac9bd10e7fc7dd27b77002b590113f14910c16a
danielmbutler <danielmbutler@hotmail.co.uk>
2021-03-04 09:12:14 +0000

    up to lecture 10

--- app/src/main/java/com/dbtechprojects/workoutapp/ExerciseActivity.kt
+++ app/src/main/java/com/dbtechprojects/workoutapp/ExerciseActivity.kt
@@ -1,22 +1,226 @@
 package com.dbtechprojects.workoutapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Dialog
+import android.media.MediaPlayer
+import android.net.Uri
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.speech.tts.TextToSpeech
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.dbtechprojects.workoutapp.R
+import com.sevenminuteworkout.Constants
+import com.sevenminuteworkout.ExerciseModel
 import kotlinx.android.synthetic.main.activity_exercise.*
+import java.util.*
+import kotlin.collections.ArrayList
 
-class ExerciseActivity : AppCompatActivity() {
+class ExerciseActivity : AppCompatActivity(), TextToSpeech.OnInitListener  {
+
+    private var restTimer: CountDownTimer? =
+            null // Variable for Rest Timer and later on we will initialize it.
+    private var restProgress =
+            0 // Variable for timer progress. As initial the rest progress is set to 0. As we are about to start.
+
+    private var exerciseTimer: CountDownTimer? = null // Variable for Exercise Timer and later on we will initialize it.
+    private var exerciseProgress = 0 // Variable for exercise timer progress. As initial the exercise progress is set to 0. As we are about to start.
+
+    private var currentExercisePosition = -1 // Current Position of Exercise.
+
+    private var tts: TextToSpeech? = null // Variable for Text to Speech
+
+    private var player: MediaPlayer? = null // Created a varible for Media Player to use later.
+    private var exerciseList: ArrayList<ExerciseModel>? = null
+
+    // Declaring a exerciseAdapter object which will be initialized later.
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_exercise)
 
         setSupportActionBar(Toolbar_excersise_activity)
-        val actionbar = supportActionBar
-        if(actionbar != null){
-            actionbar.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        // Navigate the activity on click on back button of action bar.
+        Toolbar_excersise_activity.setNavigationOnClickListener {
+           onBackPressed()
         }
 
-        Toolbar_excersise_activity.setNavigationOnClickListener {
-            onBackPressed()
+        tts = TextToSpeech(this, this)
+
+
+
+        setupRestView() // REST View is set in this function
+
+        exerciseList = Constants.defaultExerciseList()
+
+        // setting up the exercise recycler view
+
+    }
+
+    /**
+     * Here is Destroy function we will reset the rest timer to initial if it is running.
+     */
+    public override fun onDestroy() {
+        if (restTimer != null) {
+            restTimer!!.cancel()
+            restProgress = 0
+        }
+
+        if (exerciseTimer != null) {
+            exerciseTimer!!.cancel()
+            exerciseProgress = 0
+        }
+
+        if (tts != null) {
+            tts!!.stop()
+            tts!!.shutdown()
+        }
+
+        if(player != null){
+            player!!.stop()
+        }
+        super.onDestroy()
+    }
+
+    /**
+     * This the TextToSpeech override function
+     *
+     * Called to signal the completion of the TextToSpeech engine initialization.
+     */
+    override fun onInit(status: Int) {
+
+        if (status == TextToSpeech.SUCCESS) {
+            // set US English as language for tts
+            val result = tts!!.setLanguage(Locale.US)
+
+            if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                Log.e("TTS", "The Language specified is not supported!")
+            }
+
+        } else {
+            Log.e("TTS", "Initialization Failed!")
         }
     }
+
+    /**
+     * Function is used to set the timer for REST.
+     */
+    private fun setupRestView() {
+
+
+        // Here according to the view make it visible as this is Rest View so rest view is visible and exercise view is not.
+        llRestView.visibility = View.VISIBLE
+        llExcersiseView.visibility = View.GONE
+
+        /**
+         * Here firstly we will check if the timer is running the and it is not null then cancel the running timer and start the new one.
+         * And set the progress to initial which is 0.
+         */
+        if (restTimer != null) {
+            restTimer!!.cancel()
+            restProgress = 0
+        }
+
+        // Here we have set the upcoming exercise name to the text view
+        // Here as the current position is -1 by default so to selected from the list it should be 0 so we have increased it by +1.
+
+        // This function is used to set the progress details.
+        setRestProgressBar()
+    }
+
+    /**
+     * Function is used to set the progress of timer using the progress
+     */
+    private fun setRestProgressBar() {
+
+        progressBar.progress = restProgress // Sets the current progress to the specified value.
+
+        /**
+         * @param millisInFuture The number of millis in the future from the call
+         *   to {#start()} until the countdown is done and {#onFinish()}
+         *   is called.
+         * @param countDownInterval The interval along the way to receive
+         *   {#onTick(long)} callbacks.
+         */
+        // Here we have started a timer of 10 seconds so the 10000 is milliseconds is 10 seconds and the countdown interval is 1 second so it 1000.
+        restTimer = object : CountDownTimer(10000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                restProgress++ // It is increased to ascending order
+                progressBar.progress = 10 - restProgress // Indicates progress bar progress
+                tvTimer.text =
+                        (10 - restProgress).toString()  // Current progress is set to text view in terms of seconds.
+            }
+
+            override fun onFinish() {
+                currentExercisePosition++
+                setupExerciseView()
+            }
+        }.start()
+    }
+
+    /**
+     * Function is used to set the progress of timer using the progress for Exercise View.
+     */
+    private fun setupExerciseView() {
+
+        // Here according to the view make it visible as this is Exercise View so exercise view is visible and rest view is not.
+        llRestView.visibility = View.GONE
+        llExcersiseView.visibility = View.VISIBLE
+
+        /**
+         * Here firstly we will check if the timer is running the and it is not null then cancel the running timer and start the new one.
+         * And set the progress to initial which is 0.
+         */
+        if (exerciseTimer != null) {
+            exerciseTimer!!.cancel()
+            exerciseProgress = 0
+        }
+
+        /**
+         * Here current exercise name and image is set to exercise view.
+         */
+
+        setExerciseProgressBar()
+    }
+
+    /**
+     * Function is used to set the progress of timer using the progress for Exercise View for 30 Seconds
+     */
+    private fun setExerciseProgressBar() {
+
+        progressBarExcersise.progress = exerciseProgress
+
+        exerciseTimer = object : CountDownTimer(30000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                exerciseProgress++
+                progressBarExcersise.progress = 30 - exerciseProgress
+                tvExerciseTimer.text = (30 - exerciseProgress).toString()
+            }
+
+            override fun onFinish() {
+
+                    Toast.makeText(
+                            this@ExerciseActivity,
+                            "Congratulations! You have completed the 7 minutes workout.",
+                            Toast.LENGTH_SHORT
+                    ).show()
+
+            }
+        }.start()
+    }
+
+    /**
+     * Function is used to speak the text what we pass to it.
+     */
+    private fun speakOut(text: String) {
+        tts!!.speak(text, TextToSpeech.QUEUE_FLUSH, null, "")
+    }
+
+
+
+
+
 }
