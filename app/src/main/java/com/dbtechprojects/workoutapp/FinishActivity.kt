8e0c19aaca9bd68761f1a6300f061f7d8e1670dd
danielmbutler <danielmbutler@hotmail.co.uk>
2021-03-07 14:55:39 +0000

    app finished

--- app/src/main/java/com/dbtechprojects/workoutapp/FinishActivity.kt
+++ app/src/main/java/com/dbtechprojects/workoutapp/FinishActivity.kt
@@ -2,7 +2,11 @@ package com.dbtechprojects.workoutapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.sevenminuteworkout.SqliteOpenHelper
 import kotlinx.android.synthetic.main.activity_finish.*
+import java.text.SimpleDateFormat
+import java.util.*
 
 class FinishActivity : AppCompatActivity() {
 
@@ -21,5 +25,25 @@ class FinishActivity : AppCompatActivity() {
         btnFinish.setOnClickListener {
             finish()
         }
+
+        addDateToDatabase()
+
+    }
+
+    private fun addDateToDatabase(){
+        val calendar = Calendar.getInstance()
+        val dateTime = calendar.time
+        Log.i("FinishActivity", dateTime.toString())
+
+
+        val sdf = SimpleDateFormat("dd MMM, yyyy HH:mm:ss", Locale.getDefault())
+        val date = sdf.format(dateTime)
+        Log.i("FinishActivity", date.toString())
+
+        val dbhandler = SqliteOpenHelper(this, null)
+
+        dbhandler.addDate(date)
+
+
     }
 }
